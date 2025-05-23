d7cc00ee143558296af996a78db1f06caba3ae5f
Helith Jayasuriya <helithjayasuriya77@gmail.com>
2025-05-23 20:19:55 +0530

    Add files via upload

---
+++ exceptions/BookNotFoundExceptionMapper.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bookstore.exceptions;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.HashMap;
+import java.util.Map;
+
+@Provider
+public class BookNotFoundExceptionMapper implements ExceptionMapper<BookNotFoundException> {
+    @Override
+    public Response toResponse(BookNotFoundException exception) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "Book Not Found");
+        error.put("message", exception.getMessage());
+        return Response.status(Response.Status.NOT_FOUND)
+                .entity(error)
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
+}
---
+++ exceptions/CustomerNotFoundExceptionMapper.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bookstore.exceptions;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.HashMap;
+import java.util.Map;
+
+@Provider
+public class CustomerNotFoundExceptionMapper implements ExceptionMapper<CustomerNotFoundException> {
+    @Override
+    public Response toResponse(CustomerNotFoundException exception) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "Customer Not Found");
+        error.put("message", exception.getMessage());
+        return Response.status(Response.Status.NOT_FOUND)
+                .entity(error)
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
+}
